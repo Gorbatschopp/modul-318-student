95e7c6c7b838d45c551410794aa0be2cd1f0c0f4
Gorbatschopp <45390883+Gorbatschopp@users.noreply.github.com>
2018-12-04 14:53:07 +0100

    Add files via upload

---
+++ TrainProjectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrainProjectTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrainProjectTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81581df8-e7ed-4514-9a03-806209582322")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TrainProjectTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit;
+using NUnit.Framework;
+using TrainProject;
+
+namespace TrainProjectTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        readonly Format Format = new Format();
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestCase("2018-11-27T09:07:00+0100", ExpectedResult = "2018.11.27 09:07")]
+        [TestCase("2018-02-28T11:07:00+0100", ExpectedResult = "2018.02.28 11:07")]
+        [TestCase("2016-02-29T23:07:00+0100", ExpectedResult = "2016.02.29 23:07")]
+        [TestCase("2000-02-28T01:07:00+0100", ExpectedResult = "2000.02.28 01:07")]
+        public string TestFormatDateCorrectly(string time)
+        {
+            // arrange
+            string retrievedTime = Format.FormatDateCorrectly(time);
+
+            // act
+
+            // assert
+            return retrievedTime;
+        }
+
+        [TestCase("23,343434", ExpectedResult = "23.343434")]
+        [TestCase("23.343434", ExpectedResult = "23.343434")]
+        [TestCase("123,343434", ExpectedResult = "123.343434")]
+        [TestCase("1,121212", ExpectedResult = "1.121212")]
+        public string TestFormatCoordinatesCorrectly(string coord)
+        {
+            // arrange
+            string retrievedCoords = Format.FormatCoordinatesCorrectly(coord);
+
+            // act
+
+            // assert
+            return retrievedCoords;
+        }
+    }
+}
